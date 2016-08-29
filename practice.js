262eee843670ac6ce7050a41c98aa089dc2ecd0c
JJ Crossman <jj.crossman@gmail.com>
2016-08-29 14:10:08 -0500

    working on object-fiddles

--- practice.js
+++ practice.js
@@ -1,10 +1,16 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
 
-//Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
+//Create an object called me. Give it a key of name with the value being your name,
+// and another key of age with the value being your age. Then alert your name using dot notation.
 
   //Code here
-
+// var me = {
+//   name: "JJ"
+//   , age: 28
+// };
+//
+// alert( me.name );
 
 
 
@@ -13,21 +19,34 @@
 
 
 
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
+//Make a 'favoriteThings' object that contains the following keys: band, food,
+// person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
   //Code here
-
-
-//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
-
-  //Code here
-
-
-//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
-
-  //Code here
-
-
+// var favoriteThings = {
+//   band: "Billy Joel"
+//   , food: "Pizza"
+//   , person: "Uhhh"
+//   , book: "The Better Angels of Our Nature: Why Violence Has Declined"
+//   , movie: "The Count of Monte Cristo"
+//   , holiday: "National Don't Go to Work Day"
+// }
+//
+// //After you've made your object, add another key named 'car'
+// //with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+//
+//   //Code here
+// favoriteThings.car = "Fiat Panda";
+// favoriteThings.brand = "Great Value";
+//
+// //Now change the food key in your favoriteThings object to be 'Lettuce' and
+// //change the book key in your favoriteThings object to be '50 Shades of Gray'.
+//
+//   //Code here
+// favoriteThings.food = "Lettuce";
+// favoriteThings.book = "50 Shades of Gray";
+//
+// console.log ( favoriteThings );
 
 
 //NEXT PROBLEM
@@ -43,33 +62,19 @@ Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
   //Code here
-
-//After you do the above, alert your entire backPack object.
-
-  //Code here
-
-/*You probably noticed that it just alerted [object Object].
-Alerting to see the data in your Object doesn't work so well.
-Instead, console.log your whole backPack object and then check out the console. */
-
-  //Code here
-
-
-
-
-//NEXT PROBLEM
-
-
-
-
-//Create an 'alsoMe' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
-
-  //Code Here
-
-//Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
-
-  //Code Here
-
+// var backPack = {};
+// var item = "firstPocket";
+// backPack[item] = "chapstick";
+// //After you do the above, alert your entire backPack object.
+//
+//   //Code here
+// alert( backPack );
+// /*You probably noticed that it just alerted [object Object].
+// Alerting to see the data in your Object doesn't work so well.
+// Instead, console.log your whole backPack object and then check out the console. */
+//
+//   //Code here
+// console.log( backPack );
 
 
 
@@ -78,14 +83,26 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 
 
-//Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
+//Create an 'alsoMe' object with the following properties
+//name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
 
   //Code Here
-
-//Now, loop through your album object alerting every song title individually.
-
-  //Code Here
-
+// var alsoMe = {
+//   name: "Boris"
+//   , age: 54
+//   , height: 180
+//   , gender: "male"
+//   , married: true
+//   , eyeColor: "green"
+//   , hairColor: "black"
+// }
+// //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
+//
+//   //Code Here
+//
+// for (var key in alsoMe) {
+//   alert(alsoMe[key]);
+// }
 
 
 
@@ -94,14 +111,24 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 
 
-//Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
+//Create an Object called 'album' with 5 keys named different song titles
+//that you make up, with the values being the length of each song.
 
   //Code Here
-
-//Now, loop through your states object and if the states population is greater than 30K, alert that state.
-
-  //Code Here
-
+// var album = {
+//   marioGoesToWashington: 180
+//   , luigiEatsACanoli: 210
+//   , yoshiIsFilledWithEnnui: 300
+//   , bowserDoesntCare: 220
+//   , donkeyKongGetsHisMan: 180
+// }
+// //Now, loop through your album object alerting every song title individually.
+//
+//   //Code Here
+//
+// for (var key in album) {
+//     alert(key);
+// }
 
 
 
@@ -110,23 +137,28 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 
 
-var user1 = {
-    name: 'Tyler McGinnis',
-    email: null,
-    pwHash: 'U+Ldlngx2BYQk',
-    birthday: undefined,
-    username: 'tylermcginnis33',
-    age: 0
-}
-/*Above you're given a user object. Loop through the user object checking to make sure
-that each value is truthy. If it's not truthy, remove it from the object. */
+//Create an object called states that has 5 US states as properties
+//with the values being their population (doesn't have to be accurate).
 
   //Code Here
-
-//Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
-
-  //Code Here
-
+// var states = {
+//   utah: 3000000
+//   , nevada: 8925723
+//   , texas: 893527325
+//   , maine: 90
+//   , newYork: 32537234
+// }
+//
+//
+// //Now, loop through your states object and if the states population is greater than 30K, alert that state.
+//
+//   //Code Here
+//
+// for (var key in states) {
+//   if ( states[key] > 30000 ) {
+//     alert(key);
+//   }
+// }
 
 
 
@@ -135,26 +167,72 @@ that each value is truthy. If it's not truthy, remove it from the object. */
 
 
 
-var user2 = {
-        name: 'Tyler McGinnis',
-        age: 24,
-        pwHash: 'U+Ldlngx2BYQk',
-        email: 'tylermcginnis33@gmail.com',
-        birthday: '05/02/1990',
-        username: 'tylermcginnis33',
-        sayName: function(){
-            alert('Email is : ' + this.email);
-        }
-};
-//Let's say I, the user, decided to change my name and email address to the following
-// name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
+// var user1 = {
+//     name: 'Tyler McGinnis',
+//     email: null,
+//     pwHash: 'U+Ldlngx2BYQk',
+//     birthday: undefined,
+//     username: 'tylermcginnis33',
+//     age: 0
+// }
+// /* Above you're given a user object. Loop through the user object checking to make sure
+// that each value is truthy. If it's not truthy, remove it from the object. */
+//
+//   //Code Here
+//
+// for (var key in user1) {
+//   if (!!user1[key] !== true) {
+//     delete user1[key];
+//   }
+// }
+//
+// console.log( user1 );
+//
+// //Once you get your truthy Object, Change the remaining values in the object to
+// //be specific to you (name: 'your name', username: 'your username'), rather than my information.
+//
+//   //Code Here
+//
+// for (var key in user1) {
+//   if (key === "name") {
+//     user1[key] = "JJ";
+//   }
+//   else if (key === "username") {
+//     user1[key] = "mediocreExcellence";
+//   }
+// }
+//
+// console.log( user1 );
 
-  //Code Here
 
-//Now call the sayName method that's on the user object which will alert the users email
 
-  //Code Here
+//NEXT PROBLEM
 
+
+
+
+// var user2 = {
+//         name: 'Tyler McGinnis',
+//         age: 24,
+//         pwHash: 'U+Ldlngx2BYQk',
+//         email: 'tylermcginnis33@gmail.com',
+//         birthday: '05/02/1990',
+//         username: 'tylermcginnis33',
+//         sayName: function(){
+//             alert('Email is : ' + this.email);
+//         }
+// };
+// //Let's say I, the user, decided to change my name and email address to the following
+// // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
+//
+//   //Code Here
+// user2.name = "Tyler S. McGinnis";
+// user2.email = "tyler.mcginnis@devmouta.in";
+//
+// //Now call the sayName method that's on the user object which will alert the users email
+//
+//   //Code Here
+// user2.sayName();
 
 
 
